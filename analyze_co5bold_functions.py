dc0ec3f54d0ed7f65eddbbba13f59072b1b5cccc
jwiegert <joachimwiegert.astro@gmail.com>
2022-04-01 10:49:52 +0200

    finished writing createstar func

--- analyze_co5bold_functions.py
+++ analyze_co5bold_functions.py
@@ -13,11 +13,15 @@ import analyze_r3d_functions as a3d
 # Define useful numbers
 AUcm = cython.declare(cython.float ,1.49598e13) # cm
 
+# Cython info: might be needed later
+# @cython.cfunc
+# this decorator when declaring what's included in the functions
+# @cython.locals(a=cython.int)
+# AUcm = cython.declare(cython.float ,1.49598e13) # cm
+
+
+
 # ============================================================
-
-# TODO
-# do I need a function that extracts things like cellsizes also?
-
 
 # Load co5bold grid cell coordinates
 def load_grid_coordinates(
@@ -90,6 +94,8 @@ def load_grid_cellsizes(
 
     # Extract minimum grid size
     # TODO change this later to lists/arrays with cell sizes instead
+    # might not be necessary, it depends on the style of the larger
+    # c5d grids
     cellsize = (min(cellsizesx) + min(cellsizesy) + min(cellsizesz))/3
 
     return cellsize,cellcourners
@@ -127,47 +133,55 @@ def load_star_densities(
     return c5dstar_densities
 
 
-# TODO
-# Extract co5bold densities to r3d grid
-# create grid in this function also? NO - just use the pre-made function
-# But also create a function that gives numbers from c5d-grid that can
-# be used for r3d
 
 
-# nc5dedge = np.size(c5dgrid[:,0])
-# c5dstar_densities = a5d.load_star_densities()
+
+
+# Extract and create input data with
+# TODO duststar densities
+# TODO duststar temperatures
+# TODO duststar opacities
+
 
 
 def create_star(
         # All necessary inputs
-        # path to 
+        # paths!
+        savpath:str='../co5bold_data/dst28gm06n056/st28gm06n056_140.sav',
+        amrpath:str='../amr_grid.inp',
+        gridpath:str='../grid_distances.csv',
+        sizepath:str='../grid_cellsizes.csv'
     ):
+    """
+    Extracts data from Co5bold sav-files and creates a dust_star for Radmc3d.
 
+    INPUT
+    -----
+    amrpath: path to amr_grid.inp
+    gridpath: path to grid_distances.csv
+    sizepath: path to grid_cellsizes.csv
+    savpath: path to co5bold sav-file
+
+    OUTPUT
+    ------
+    R3D density file: dust_density_star.inp
+    """
 
     # Load R3D grid
     print('Loading R3D grid')
-    # amrpath:str='../amr_grid.inp'
-    nxyz,nrefines,nleafs,nbranch,gridedge = a3d.load_gridprops()
-    # gridpath:str='../grid_distances.csv',
-    # amrpath:str='../amr_grid.inp',
-    r3ddistances = a3d.load_griddistances()
-    # sizepath:str='../grid_cellsizes.csv',
-    # amrpath:str='../amr_grid.inp',
-    r3dcellsizes = a3d.load_cellsizes()
+    nleafs = a3d.load_gridprops(amrpath=amrpath)[2]
+    r3ddistances = a3d.load_griddistances(amrpath=amrpath,gridpath=gridpath)
+    r3dcellsizes = a3d.load_cellsizes(amrpath=amrpath,sizepath=sizepath)
 
     # Load C5D grid
     print('Loading C5D grid properties')
-    # savpath:str='../co5bold_data/dst28gm06n056/st28gm06n056_140.sav'
-    # perhaps also the name of the dict-parameter
-    c5dcellsize,c5dcourners = load_grid_cellsizes()
-    # savpath:str='../co5bold_data/dst28gm06n056/st28gm06n056_140.sav'
-    c5dgrid = load_grid_coordinates()
-    nc5dedge = np.size(c5dgrid[:,0])
+    # TODO perhaps also the name of the dict-parameter
+    c5dcellsize = load_grid_cellsizes(savpath=savpath)[0]
+    c5dgrid = load_grid_coordinates(savpath=savpath)
 
     # Load C5D densities
     print('Loading C5D star-densities')
-    # savpath:str='../co5bold_data/dst28gm06n056/st28gm06n056_140.sav'
-    c5dstar_densities = load_star_densities()
+    c5dstar_densities = load_star_densities(savpath=savpath)
 
 
     # Start working :)
@@ -214,31 +228,65 @@ def create_star(
             # Number of c5dcells within r3dcell
             nchildcells = c5dxrange.size*c5dyrange.size*c5dzrange.size
 
-        # TODO HÄR ÄR JAG
+            # Check so that there are any c5dcells within r3dcell
+            if nchildcells < 0:
 
+                # When r3dcellsize ~ c5dcellsize the loop misses the c5dcells inside r3dcells
+                # due to numerical errors. So here I increase the spatial range to loop through
+                # otherwise I get a lot of zero-cells.
+                temprange = [r3dxrange[0]*adaptive_range,r3dxrange[1]*adaptive_range]
+                c5dxrange = np.argwhere(temprange[0] <= c5dgrid[np.argwhere(c5dgrid[:,0] <= temprange[1]),0])[:,0]
+                temprange = [r3dyrange[0]*adaptive_range,r3dyrange[1]*adaptive_range]
+                c5dyrange = np.argwhere(temprange[0] <= c5dgrid[np.argwhere(c5dgrid[:,1] <= temprange[1]),1])[:,0]
+                temprange = [r3dzrange[0]*adaptive_range,r3dzrange[1]*adaptive_range]
+                c5dzrange = np.argwhere(temprange[0] <= c5dgrid[np.argwhere(c5dgrid[:,2] <= temprange[1]),2])[:,0]
 
+                # New number of c5dcells within r3dcell
+                nchildcells = c5dxrange.size*c5dyrange.size*c5dzrange.size
 
+            # Then loop through c5dcells within r3dcell
+            for nnz in c5dzrange:
+                for nny in c5dyrange:
+                    for nnx in c5dxrange:
 
+                        # Sum all densities
+                        stararray[nr3d] += c5dstar_densities[nnx,nny,nnz]
 
+            # Average the density of each r3dcell by number of c5dcells
+            stararray[nr3d] /= nchildcells
 
+            # Some progress bar info
+            if int(nr3d/nleafs*100) == 25 and progbar == 0:
+                progbar += 1
+                print('Finished 25 per cent of the grid.')
 
+            if int(nr3d/nleafs*100) == 50 and progbar == 1:
+                progbar += 1
+                print('Finished 50 per cent of the grid.')
 
-    # Extract and create input data with
-    # TODO duststar densities
-    # TODO duststar temperatures
-    # TODO duststar opacities
+            if int(nr3d/nleafs*100) == 75 and progbar == 2:
+                progbar += 1
+                print('Finished 75 per cent of the grid.')
 
-    return 'hej'
+        print('Writing dust_density_star.inp')
 
+        # Open density file
+        with open('../dust_density_star.inp', 'w') as f:
+            # dust and open('../dust_temperature_star.inp', 'w') as ftemp
 
+            # Write header of dust_densities_star.inp
+            # 1
+            # nleafs
+            # number dust species
+            f.write(f'1\n{int(nleafs)}\n1\n')
 
-# @cython.cfunc
-# this decorator when declaring what's included in the functions
-# @cython.locals(a=cython.int)
-#
+            # Do I need to have an array? Can I incorporate this above?
+            # Or if I want to have several data at once, perhaps this needs
+            # to be an array?
+            for nr3d in range(nleafs):
+                f.write(f'{stararray[nr3d]}\n')
 
-
-
+    print('C5D Dust star: done.\n')
 
 
 
