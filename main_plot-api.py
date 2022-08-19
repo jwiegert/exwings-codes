7d812db93c88793b9f7016d21c17307afbf925cd
jwiegert <joachimwiegert.astro@gmail.com>
2022-08-19 16:43:13 +0200

    start work on api

--- main_plot-api.py
+++ main_plot-api.py
@@ -10,3 +10,41 @@
 
 
 
+#    API't ska automatiskt lista alla kataloger i r3dresults, sen alla i den man väljer där, och sen alla sed-filer, spectrum*.out och alla bildfiler, image*.out
+#    Patherna innehåller info om modell, fas, inclination och våglängder
+#    Plotta alla bilder med samma inklination, plotta alla bilder med samma våglängd
+#    Plotta alla SEDer med samma inklination i samma figur
+#        os.listdir(path='../r3dresults/st28gm06n056/140/')    
+#    eller
+#        import glob
+#        glob.glob('../r3dresults/st28gm06n056/140/image*.out')
+#    och för kataloger
+#        glob.glob('../r3dresults/st28gm06n056/*/')
+#    Ger då listor som kan vara inputs till mina menyer
+
+
+import os
+
+path='../r3dresults/'
+
+
+# List all folders in r3dresults:
+modelnames = [modelname for modelname in os.listdir(path=path) if os.path.isdir(path+modelname) == True]
+modelnames.sort()
+
+# List all folders in chosen folder
+# TODO chose folder, done in API...
+#      this is a test choice
+modelname = modelnames[-1]+'/'
+
+phases = [phase for phase in os.listdir(path=path+modelname) if os.path.isdir(path+modelname+phase) == True]
+
+
+#for modelname in modelnames:
+#    phases.append(os.listdir(path=f'../r3dresults/{modelname}'))
+
+print(modelnames)
+print()
+print(phases)
+
+
