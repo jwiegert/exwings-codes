69810ddd5a5310767366892778209b0c473db729
jwiegert <joachimwiegert.astro@gmail.com>
2022-10-31 17:32:29 +0100

    finished optool-script-writer

--- create_r3d_functions.py
+++ create_r3d_functions.py
@@ -1,7 +1,9 @@
 # Functions for creating input data for R3d simulations for Exwings project
 # ------------------------------------------------------------ #
 # Various useful packages
+from asyncio import LimitOverrunError
 import sys
+import wave
 import numpy as np
 import scipy as s
 from scipy.integrate import quad
@@ -778,15 +780,21 @@ def create_wavelength(
 # ------------------------------------------------------------ #
 # Opacity-functions
 
+
+
+# TODO
+# a function that loads and extracts and returns a grain size list from 
+# the binned grain szies as created in c5d binning of grain sizes
+
 # Create optool-kappa-file-script
 def create_optoolscript(
+        wavelength_path:str='../wavelength_micron.inp',
         grainum_sizes:list=[0.1],
-        grainsize_type:str='normal',
+        grainsize_type:str='lognormal',
         grainsize_na:int=21,
         grainsize_power:float=-3.5,
         specie:str='mg2sio4',
         grain_type:str='mie'
-
     ):
     """
 
@@ -811,49 +819,24 @@ def create_optoolscript(
     
     """
 
-    # grain_type:str='mie' or 'dhs', for distr of hollow spheres
-
-
-    lnk_path = f'../my_lnk_data/{specie}.lnk'
+    lnk_path = f'~/program/optool/my_lnk_data/{specie}.lnk'
 
     # Load wavelenth_grid from wavelength.inp-file and use for the wavelengt-settings!
-
-
-    # For Mie theory, 
-    # MRN-distribution, 3.5 means a^-3.5, minus is dropped
-    # -a AMIN [AMAX [APOW [NA]]]
-    # optool -c ../my_lnk_data/mg2sio4.lnk -mie -a 0.04725051 0.89663067 3.5 10 -lmin 0.1 -lmax 1000 -nlam 100 -s -radmc mg2sio4_optooltest
-
-
-
-    # for log-normal-grain-size distribution:
-    # if it's a list:
-    # compute amin-max from log-mean-grainsizes between the mean-grainsizes
-    #
-    # if it's a singular size, take 0.1 and 10 of the mean
-
-
-
-    # sätt amin till att vara mitt-emellan föregående agrain och nästa
-    # alternativt, mitt-emellan i log-space? i många fall blir det ungefär samma
-    # för "ändarna" i listan kan det funka att bara köra med 0.5*agrain_min och 1.5*agrain_max
-    # amax för en storlek blir amin för nästa
-
-    # log-normal distribution (same as I've used before)
-    # -a amin amax amean:asig [na]
-    #optool 
-    # -c ../my_lnk_data/mg2sio4.lnk 
-    # -mie 
-    # -a 0.70787952 0.89663067 0.80225509:0.1 11 -lmin 0.1 -lmax 1000 -nlam 100 -s -radmc mg2sio4_optooltest
+    wavelengths,nwave = a3d.load_wavelengthgrid(
+        path=wavelength_path
+    )
+    # Wavelength grid limits
+    lmin = min(wavelengths)
+    lmax = max(wavelengths)
 
 
     # MRN-dist needs Amin, Amax, power and number of sizez
     #   amin, max: given from amean
     #   power and na needs to be given : 2 numbers
+    #
     # Lognormal needs Amin, Amax, Amean, Asig (always the same?), and number of sizes
     #   amin, amax, asig, given from amean
     #   needs number of sizes : 1 number
-
 
 
     # Define grain size limits
@@ -874,51 +857,79 @@ def create_optoolscript(
         
         amax_um.append(grainum_sizes[-1] + grainum_sizes[-1] - amax_um[-1])
 
-        # Define sigma
-        # Sigma can be defined from amin-max, FWHM = 2.355 sigma
-        # set FWHM = amax-amin
-        # sigma = (amax-amin) / 2.355
-        # ie I want the HALF of the HEIGHT of the Gaussian to be "connected" to the next
-        # grain size Gaussian.
-        # This can ofcourse be varied
-        # And with the max-min-sizes there won't be any problems or overlaps anyway
-        #
-        # Because of the possibility of having log-grain-sizes I should make a list of asig also
 
-        asigma_um = []
-
-        for nn in range(len(grainum_sizes)):
-            asigma_um.append( (amax_um[nn] - amin_um[nn])/2.355 )
-
-
-
+        # Check if we have lognormal distr
+        if grainsize_type == 'lognormal':
+            #
+            # Define sigma:
+            #
+            # Sigma can be defined from amin-max and that FWHM = 2.355 sigma
+            #
+            # set FWHM = amax-amin
+            # sigma = (amax-amin) / 2.355
+            #
+            # ie we want the HALF of the HEIGHT of the Gaussian to be "connected" to the next
+            # grain size Gaussian.
+            # This can ofcourse be varied
+            # With the max-min-sizes there won't be any overlaps
+            #
+            # Because of the possibility of having log-grain-sizes I should make a list of asig also
+            #
+            asigma_um = []
+            for nn in range(len(grainum_sizes)):
+                asigma_um.append( (amax_um[nn] - amin_um[nn])/2.355 )
 
     else:
         # If there is only one size, plus minus half of it
         amin_um = [0.5*grainum_sizes[0]]
         amax_um = [grainum_sizes[0] + amin_um[0]]
 
-        # Then sigma is
-        asigma_um = [(amax_um[0] - amin_um[0])/2.355]
+        if grainsize_type == 'lognormal':
+            # Then sigma is just
+            asigma_um = [(amax_um[0] - amin_um[0])/2.355]
 
 
 
+    with open('../optool_script.sh','w') as f:
 
-    # Write the script with optool commands
+        # Write header
+        f.write(f'# Script to run optool to create kappa-files for R3D\n#\n')
+        f.write(f'#    Grain sizes (um): {grainum_sizes} \n')
+        f.write(f'#    Grain size distribution-style: {grainsize_type}\n')
+        f.write(f'#    Grain type: {grain_type}\n')
+        f.write(f'#    Dust specie: {specie}\n#\n')
 
-    for nn in range(len(grainum_sizes)):
-        #print(f'{amin_um[nn]} - {grainum_sizes[nn]} - {amax_um[nn]}: sigma: {asigma_um[nn]}')
+        # Write the script with optool commands
+        for nn in range(len(grainum_sizes)):
+
+            # Extract data to make the codes clearer
+
+            # Grain size limits
+            amin = amin_um[nn]
+            amax = amax_um[nn]
+
+            # TODO Write into an optool-bash-script
+
+            if grainsize_type == 'lognormal':
         
-        # Change units to cm
-        amin = amin_um[nn]
-        amax = amax_um[nn]
-        amean = grainum_sizes[nn]
-        asigma = asigma_um[nn]
+                # Extract data to make the codes clearer
+                amean = grainum_sizes[nn]
+                asigma = asigma_um[nn]
 
-        print(f'optool -c {lnk_path} -{grain_type} -a {amin} {amax} {amean}:{asigma} {grainsize_na} -lmin 0.1 -lmax 1000 -nlam 100 -s -radmc mg2sio4_{grainum_sizes[nn]}')
+                # log-normal distribution (same as I've used before)
+                # -a amin amax amean:asig [na]
+                f.write(f'optool -c {lnk_path} -{grain_type} -a {amin} {amax} {amean}:{asigma} {grainsize_na} -lmin {lmin} -lmax {lmax} -nlam {nwave} -s -radmc mg2sio4_{grainum_sizes[nn]}\n')
+            
+            if grainsize_type == 'mrn':
+                # For Mie theory, 
+                # MRN-distribution, 3.5 means a^-3.5, minus is dropped
+                # -a AMIN AMAX APOW NA
+                f.write(f'optool -c {lnk_path} -{grain_type} -a {amin} {amax} {-grainsize_power} {grainsize_na} -lmin 0.1 -lmax 1000 -nlam 100 -s -radmc mg2sio4_{grainum_sizes[nn]}\n')
 
+    # Make into executable
+    os.system('chmod +x ../optool_script.sh')
 
-
+    print('create_optoolscript:\n    ../optool_script.sh\nDONE\n')
 
 
 
