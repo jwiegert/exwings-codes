a44ab23bd76f8d7740b2515a9523486d8533bffc
jwiegert <joachimwiegert.astro@gmail.com>
2023-03-03 15:35:16 +0100

    checked and cinluded darwinopacities

--- analyze_darwin_functions.py
+++ analyze_darwin_functions.py
@@ -57,35 +57,36 @@ def load_darwindata(
       average_temperature: np.array with time-averaged radial dependant gas temperatures in K
     """
 
-    filename = f'../darwin_data/{modelname}_sel/model.{timestep}.dat'
+    filename = f'../darwin_data/{modelname}_kappaross/model.{timestep}.rtk'
 
 
     radius_cm = []
     gas_density = []
     gas_temperature = []
-    # TODO gas_opacity
+    gas_opacity = []
 
     with open(filename, 'r') as f:
         for nn,line in enumerate(f.readlines()):
             # 18 lines are the header
-            if nn > 18:
+            if nn > 8:
                 
                 # Clean up each line
                 line_list = line.strip().split('  ')
 
-                # Save data
-                radius_cm.append(float(line_list[0]))
-                gas_density.append(float(line_list[1]))
-                gas_temperature.append(float(line_list[2]))
-                # TODO gas_opacity
+                if len(line_list) > 1:
+                    # Save data
+                    radius_cm.append(float(line_list[0]))
+                    gas_density.append(float(line_list[1]))
+                    gas_temperature.append(float(line_list[2]))
+                    gas_opacity.append(float(line_list[3]))
 
-    # Save in arrays and reverse the radial direction
+    # Save in arrays and reverse the radial direction to get growing radius
     radius_cm = np.array(radius_cm[::-1])
     gas_density = np.array(gas_density[::-1])
     gas_temperature = np.array(gas_temperature[::-1])
-    # TODO all_opacities
+    gas_opacity = np.array(gas_opacity[::-1])
 
-    return radius_cm,gas_density,gas_temperature
+    return radius_cm,gas_density,gas_temperature,gas_opacity
 
 
 # Take all time steps for model and make time-averaged data
@@ -106,7 +107,7 @@ def load_averagedarwindata(
     """
 
     # Given modelname, list all time-step-files
-    filenames = os.listdir(f'../darwin_data/{modelname}_sel/')
+    filenames = os.listdir(f'../darwin_data/{modelname}_kappaross/')
     Ntimesteps = len(filenames)
 
 
@@ -115,32 +116,33 @@ def load_averagedarwindata(
     all_radii = []
     all_density = []
     all_temperature = []
-    # TODO all_opacities
+    all_opacity = []
 
     for filename in filenames:
         
         #Loop through files
-        path = f'../darwin_data/{modelname}_sel/{filename}'
+        path = f'../darwin_data/{modelname}_kappaross/{filename}'
 
         # Reset lists
         radius_cm = []
         gas_density = []
         gas_temperature = []
-        # TODO gas_opacity
+        gas_opacity = []
 
         with open(path, 'r') as f:
             for nn,line in enumerate(f.readlines()):
                 # 18 lines are the header
-                if nn > 18:
+                if nn > 8:
                     
                     # Clean up each line
                     line_list = line.strip().split('  ')
 
-                    # Save data
-                    radius_cm.append(float(line_list[0]))
-                    gas_density.append(float(line_list[1]))
-                    gas_temperature.append(float(line_list[2]))
-                    # TODO gas_opacity
+                    if len(line_list) > 1:
+                        # Save data
+                        radius_cm.append(float(line_list[0]))
+                        gas_density.append(float(line_list[1]))
+                        gas_temperature.append(float(line_list[2]))
+                        gas_opacity.append(float(line_list[3]))
 
         # Save a list of number of radial cells per time step
         Ncells.append(len(radius_cm))
@@ -149,8 +151,7 @@ def load_averagedarwindata(
         all_radii.append(radius_cm[::-1])
         all_density.append(gas_density[::-1])
         all_temperature.append(gas_temperature[::-1])
-        # TODO all_opacities
-    
+        all_opacity.append(gas_opacity[::-1])
 
 
     # Average all data into one time-averaged set
@@ -164,8 +165,9 @@ def load_averagedarwindata(
     )
     average_density = np.zeros(Naverage)
     average_temperature = np.zeros(Naverage)
+    average_opacity = np.zeros(Naverage)
     average_cellcounter = np.zeros(Naverage)
-    # TODO average_opacity
+
 
     # Loop over all time steps
     for nstep in range(Ntimesteps):
@@ -180,7 +182,7 @@ def load_averagedarwindata(
                     # Sum all data at each radial point
                     average_density[na] += all_density[nstep][nn]
                     average_temperature[na] += all_temperature[nstep][nn]
-                    # TODO average_opacity
+                    average_opacity[na] += all_opacity[nstep][nn]
                     average_cellcounter[na] += 1
 
 
@@ -193,7 +195,7 @@ def load_averagedarwindata(
     # Average the data
     average_density /= average_cellcounter
     average_temperature /= average_cellcounter
-    #TODO average_opacity /= average_cellcounter
+    average_opacity /= average_cellcounter
 
 
 
@@ -206,21 +208,16 @@ def load_averagedarwindata(
             # Take average of prev and next index (before and after "hole")
             average_density[zero_list] = 0.5*(average_density[zero_list[0]-1] + average_density[zero_list[-1]+1])
             average_temperature[zero_list] = 0.5*(average_temperature[zero_list[0]-1] + average_temperature[zero_list[-1]+1])
-            # TODO average_opacity
+            average_opacity[zero_list] = 0.5*(average_opacity[zero_list[0]-1] + average_opacity[zero_list[-1]+1])
 
         # For the end of the array, just take the final real value
         if zero_list[-1] == Naverage-1:
             average_density[zero_list] = average_density[zero_list[0]-1] 
             average_temperature[zero_list] = average_temperature[zero_list[0]-1]
-            # TODO average_opacity
+            average_opacity[zero_list] = average_opacity[zero_list[0]-1]
 
+    return average_radius,average_density,average_temperature,average_opacity
 
-    return average_radius,average_density,average_temperature
-
-
-# TODO
-# func that computes rosseland opacities from density and temperatures
-# and outputs array with density*opacity
 
 
 # TODO
@@ -229,6 +226,7 @@ def darwin_to_radmc3d(
     darwin_radius:list,
     darwin_density:list,
     darwin_temperature:list,
+    darwin_opacity:list,
     gridpath:str='../grid_distances.csv',
     amrpath:str='../amr_grid.inp'
 ):
@@ -258,10 +256,26 @@ def darwin_to_radmc3d(
     )
     r3d_radius = r3d_griddistances[:,0]
 
+
+    # Create density-opacity array for r3d-sims, since the gas-density
+    # in r3d is density*rosselandopacity, and the gas kappa is just
+    # kappaabs = 1 and kappascat = 0
+    #
+    # First check if density-opacity input data are np-arrays
+    if type(darwin_density) == list:
+        darwin_density = np.array(darwin_density)
+    if type(darwin_opacity) == list:
+        darwin_opacity = np.array(darwin_opacity)
+
+    # Then write new array
+    darwin_densityopacity = darwin_density * darwin_opacity
+
+
     # Interpolate darwin-1d-data to r3d-radial grid
     # Default setting of interp is to keep first o last value outside the range
-    r3d_darwindensity = np.interp(r3d_radius,darwin_radius,darwin_density)
+    r3d_darwindensityopa = np.interp(r3d_radius,darwin_radius,darwin_densityopacity)
     r3d_darwintemperature = np.interp(r3d_radius,darwin_radius,darwin_temperature)
+
 
     # Save in RADMC-3D inp-files
     nleafs = np.size(r3d_radius)
@@ -283,7 +297,7 @@ def darwin_to_radmc3d(
         # zip
 
         for nr3d in range(nleafs):
-            fdensity.write(f'{r3d_darwindensity[nr3d]}\n')
+            fdensity.write(f'{r3d_darwindensityopa[nr3d]}\n')
             ftemperature.write(f'{r3d_darwintemperature[nr3d]}\n')
 
             # Some progress bar info
@@ -301,3 +315,5 @@ def darwin_to_radmc3d(
 
     print(f'DARWIN Dust-star:\n    dust_density_darwinstar.inp\n    dust_temperature_darwinstar.dat\nDONE\n')
 
+    # TEMP! Just to check!
+    return darwin_densityopacity
